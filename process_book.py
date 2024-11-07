00571b3df8d05df7ad943fa22ab136ee0a371b47
davidricodias <daviricodias.dev@gmail.com>
2024-11-07 14:45:16 +0100

    Update book processing

--- process_book.py
+++ process_book.py
@@ -90,41 +90,86 @@ def __write_csv(df: pl.DataFrame, ric, date):
     df.filter((pl.col("#RIC") == ric) & (pl.col("Date") == date)).drop(["#RIC", "Date"]).rename({"Date-Time": "date"}).write_csv(filename)
 
 # %%
-book_df = pl.scan_csv(book_filename, infer_schema_length=100000, try_parse_dates=True, low_memory=True)
+book_df = pl.scan_csv(book_filename, infer_schema_length=100000, try_parse_dates=True)
 logging.info(f"Going to lazy load {book_filename}")
 # %%
 
+nanoseconds_in_a_microsecond = 1000
 book_df = (
     book_df
     .drop(["Domain", "GMT Offset", "Type", "Exch Time"])
     .filter(
         (pl.col("Date-Time").dt.time() >= time(7, 0)) &
         (pl.col("Date-Time").dt.time() <= time(15, 30))
-    )    .filter(~pl.all_horizontal(pl.col(bid_columns_to_check_all_nans).is_null()))
+    )
+    .filter(~pl.all_horizontal(pl.col(bid_columns_to_check_all_nans).is_null()))
     .filter(~pl.all_horizontal(pl.col(ask_columns_to_check_all_nans).is_null()))
-    .with_columns([pl.col(col).fill_null(0) for col in fill_null_with_zero])  # Fill null size and number of orders with zero 
-    .with_columns([pl.col(col).fill_null(strategy='forward') for col in [*bid_price_columns, *ask_price_columns]])  # Fill 
-    .with_columns(pl.col("Date-Time").dt.date().alias("Date"))
+    .with_columns(
+        [pl.col(col).fill_null(0) for col in fill_null_with_zero]
+    )  # Fill null size and number of orders with zero 
+    .with_columns(
+        [pl.col(col).fill_null(strategy='forward') for col in [*bid_price_columns, *ask_price_columns]]
+    )  # Fill 
+    .with_columns(
+        pl.col("Date-Time").dt.date().alias("Date")
+    )
+    .with_columns(pl.col("Date-Time").dt.round("500ms").alias("Date-Time"))
     .drop_nulls()
+    # .with_columns(
+    #     pl.len().over("Date-Time").alias('duplicate_datetime')
+    # )
+    # .with_columns(
+    #     pl.col("Date-Time").cum_count().over("Date-Time").alias('counter')
+    # )
+    # .with_columns(
+    #     (pl.col("Date-Time").dt.cast_time_unit('ns') + pl.duration(nanoseconds=(nanoseconds_in_a_microsecond/pl.col("duplicate_datetime"))*(pl.col("counter")-1))).alias("Date-Time")
+    # )
     .select(ordered_columns)
 ).collect()
+from datetime import timedelta
 
-logging.info(f"Filtered {book_filename}")
-logging.info(book_df.describe())
-logging.info(book_df.schema)
-logging.info(book_df.select(pl.col("#RIC").value_counts()))
-logging.info("Columns with null values")
-logging.info(book_df.null_count())
+# This snippet checks for the minimum time between events. 
+# For the 3 symbols is 1 microsecond (AENA)
+min_time_diff = 10e8
+for ric in book_df.select(["#RIC"]).unique().to_numpy():
+    df_with_diffs = book_df.filter(
+        pl.col("#RIC") == ric
+    ).with_columns([
+        (pl.col("Date-Time") - pl.col("Date-Time").shift(1)).alias("time_diff")
+    ]).with_columns(
+        pl.col("time_diff").cast(pl.Int64).alias("time_diff")
+    )
+    min_diff_ric = df_with_diffs.select(pl.col("time_diff").min()).item()
+    if min_diff_ric < min_time_diff:
+        min_time_diff = min_diff_ric
+    logging.info(f"The minimal time diff for {ric} is {min_diff_ric}")
+logging.info(f"The minimal time diff for all symbols is {min_time_diff}, {type(min_time_diff)}")
 
-# %%
+# Upsample then downsample to have equally-spaced measures
+import pandas as pd
+new_book_df = pl.DataFrame()
 for (ric, date) in book_df.select(["#RIC", "Date"]).unique().to_numpy():
-    __write_csv(book_df, ric, date)
-# threads = []
-# for (ric, date) in book_df.select(["#RIC", "Date"]).unique().to_numpy():
-#     th = threading.Thread(target=__write_csv, args=(book_df, ric, date))
-#     th.start()
-#     threads.append(th)
-# [ t.join() for t in threads ]
+    logging.info(f"Going to save csv for {ric} on {date}")
+    df = book_df.filter(
+        (pl.col("#RIC") == ric) & (pl.col("Date") == date)
+    )
+    
+    first_row = df.head(1).with_columns(
+        (pl.col("Date-Time").dt.truncate("1s")).alias("Date-Time")
+    )
+    df = pl.concat([first_row, df])
+        
+    filename = os.path.join(final_data_folder, f"{ric}_{date.strftime('%y_%m_%d')}.csv")
+
+    df = (df.unique(subset=["Date-Time"], keep="first", maintain_order=True)
+            .upsample(time_column="Date-Time", every="500ms", maintain_order=True)
+            .with_columns(pl.col(df.columns).fill_null(strategy='forward'))
+            # .select(pl.all().last())
+            # .group_by_dynamic('Date-Time', every='500us')
+            # .agg(pl.all().mean())
+            )
+    df.drop(["#RIC", "Date"]).rename({"Date-Time": "date"}).write_csv(filename)
+
 
 # %%
 logging.info(f"Finished {os.path.abspath(__file__)}")
