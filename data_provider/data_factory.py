b29c710e4b88f4a288958d4537dd9a345148bbe6
davidricodias <daviricodias.dev@gmail.com>
2024-11-07 14:45:54 +0100

    Adda dataset concatenation

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,11 +1,8 @@
-from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_Pred
-from torch.utils.data import DataLoader
+import os
+from data_provider.data_loader import Dataset_Custom
+from torch.utils.data import DataLoader, ConcatDataset
 
 data_dict = {
-    'ETTh1': Dataset_ETT_hour,
-    'ETTh2': Dataset_ETT_hour,
-    'ETTm1': Dataset_ETT_minute,
-    'ETTm2': Dataset_ETT_minute,
     'custom': Dataset_Custom,
 }
 
@@ -24,26 +21,29 @@ def data_provider(args, flag):
         drop_last = False
         batch_size = 1
         freq = args.freq
-        Data = Dataset_Pred
+        Data = Dataset_Custom
     else:
         shuffle_flag = True
         drop_last = True
         batch_size = args.batch_size
         freq = args.freq
 
-    data_set = Data(
-        root_path=args.root_path,
-        data_path=args.data_path,
-        flag=flag,
-        size=[args.seq_len, args.label_len, args.pred_len],
-        features=args.features,
-        target=args.target,
-        timeenc=timeenc,
-        freq=freq
-    )
-    print(flag, len(data_set))
+    datasets = []
+    for data_path in os.listdir(args.root_path):
+        data_set = Data(
+            root_path=args.root_path,
+            data_path=data_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len],
+            features=args.features,
+            target=args.target,
+            timeenc=timeenc,
+            freq=freq
+        )
+        datasets.append(data_set)
+    concat_dataset = ConcatDataset(datasets)
     data_loader = DataLoader(
-        data_set,
+        concat_dataset,
         batch_size=batch_size,
         shuffle=shuffle_flag,
         num_workers=args.num_workers,
