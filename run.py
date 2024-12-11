c25e2b64784c05eb9d58bbfbbbe6ad9968ca4f18
davidricodias <daviricodias.dev@gmail.com>
2024-12-11 15:33:23 +0100

    Change defaults in the run

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 def main():
-    fix_seed = 2021
+    fix_seed = 42
     random.seed(fix_seed)
     torch.manual_seed(fix_seed)
     np.random.seed(fix_seed)
@@ -15,37 +15,38 @@ def main():
     parser = argparse.ArgumentParser(description='Autoformer & Transformer family for Time Series Forecasting')
 
     # basic config
-    parser.add_argument('--is_training', type=int, default=1, help='status')
-    parser.add_argument('--model_id', type=str, default='test', help='model id')
+    parser.add_argument('--is_training', type=int, default=0, help='status')
+    parser.add_argument('--max_num_batches_per_epoch', type=int, default=10000, help='max_num_batches_per_epoch')
+    parser.add_argument('--max_training_time', type=int, default=6*60*60, help='max_training_time')  # 6 hours
+    parser.add_argument('--model_id', type=str, default='PatchMixer_for_lob_for_grifols', help='model id')
     parser.add_argument('--model', type=str, default='PatchMixer',
-                        help='model name, options: [Autoformer, Informer, Transformer]')
+                        help='model name, options: [PatchMixer]')
+    parser.add_argument('--recover', type=bool, default=True, help='Recover training')
 
     # data loader
     parser.add_argument('--data', type=str, default='custom', help='dataset type')
     parser.add_argument('--root_path', type=str, default='./dataset/lob', help='root path of the data file')
-    parser.add_argument('--data_path', type=str, default='lob.csv', help='data file')
+    parser.add_argument('--data_path', type=str, default='GRLS_lob.csv', help='data file')
     parser.add_argument('--features', type=str, default='M',
                         help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
     parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
-    parser.add_argument('--freq', type=str, default='us',
+    parser.add_argument('--freq', type=str, default='500ms',
                         help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
     parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
 
     # forecasting task
-    parser.add_argument('--seq_len', type=int, default=512, help='input sequence length')
-    parser.add_argument('--label_len', type=int, default=64, help='start token length')
-    parser.add_argument('--pred_len', type=int, default=256, help='prediction sequence length')
+    parser.add_argument('--seq_len', type=int, default=64, help='input sequence length')
+    parser.add_argument('--label_len', type=int, default=0, help='start token length')
+    parser.add_argument('--pred_len', type=int, default=4, help='prediction sequence length')
 
     # model define
-    parser.add_argument('--bucket_size', type=int, default=4, help='for Reformer')
-    parser.add_argument('--n_hashes', type=int, default=4, help='for Reformer')
-    parser.add_argument('--enc_in', type=int, default=60, help='encoder input size')
-    parser.add_argument('--dec_in', type=int, default=60, help='decoder input size')
-    parser.add_argument('--c_out', type=int, default=60, help='output size')
+    parser.add_argument('--enc_in', type=int, default=62, help='encoder input size')
+    parser.add_argument('--dec_in', type=int, default=62, help='decoder input size')
+    parser.add_argument('--c_out', type=int, default=62, help='output size')
     parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
-    parser.add_argument('--n_heads', type=int, default=4, help='num of heads')
-    parser.add_argument('--e_layers', type=int, default=4, help='num of encoder layers')
-    parser.add_argument('--d_layers', type=int, default=2, help='num of decoder layers')
+    parser.add_argument('--n_heads', type=int, default=2, help='num of heads')
+    parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+    parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
     parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
     parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
     parser.add_argument('--factor', type=int, default=3, help='attn factor')
@@ -57,7 +58,7 @@ def main():
                         help='time features encoding, options:[timeF, fixed, learned]')
     parser.add_argument('--activation', type=str, default='gelu', help='activation')
     parser.add_argument('--output_attention', action='store_true', help='whether to output attention in encoder')
-    parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
+    parser.add_argument('--do_predict', action='store_false', help='whether to predict unseen future data')
     # PatchTST
     parser.add_argument('--fc_dropout', type=float, default=0.05, help='fully connected dropout')
     parser.add_argument('--head_dropout', type=float, default=0.0, help='head dropout')
@@ -73,12 +74,12 @@ def main():
     parser.add_argument('--mixer_kernel_size', type=int, default=8, help='patchmixer-kernel')
 
     # optimization
-    parser.add_argument('--num_workers', type=int, default=1, help='data loader num workers')
+    parser.add_argument('--num_workers', type=int, default=8, help='data loader num workers')
     parser.add_argument('--itr', type=int, default=2, help='experiments times')
     parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
     parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
     parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
-    parser.add_argument('--learning_rate', type=float, default=0.000001, help='optimizer learning rate')
+    parser.add_argument('--learning_rate', type=float, default=0.00001, help='optimizer learning rate')
     parser.add_argument('--des', type=str, default='test', help='exp description')
     parser.add_argument('--loss', type=str, default='mse', help='loss function')
     parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
@@ -87,8 +88,8 @@ def main():
     # GPU
     parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
     parser.add_argument('--gpu', type=int, default=0, help='gpu')
-    parser.add_argument('--use_multi_gpu', action='store_false', help='use multiple gpus', default=False)
-    parser.add_argument('--devices', type=str, default='0,1', help='device ids of multile gpus')
+    parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=True)
+    parser.add_argument('--devices', type=str, default='0,1', help='device ids of multiple gpus')
 
     args = parser.parse_args()
 
